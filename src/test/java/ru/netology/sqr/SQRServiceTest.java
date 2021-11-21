2b9a6550063941f2166cec089131a573f3b3b1b6
TanyaUrlapkina <write2me92@mail.ru>
2021-11-21 15:01:58 +0300

    "new test SqrtValueCount"

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,18 +1,16 @@
 package ru.netology.sqr;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
 
     @org.junit.jupiter.api.Test
     void shouldSqrtValueCount() {
-            SQRService service = new SQRService();
+            SQRService run = new SQRService();
             int lowLim=200;
             int upLim=300;
             int expected=3;
-            int actual = service.SqrtValueCount(lowLim,upLim);
+            int actual = run.SqrtValueCount(lowLim,upLim);
             assertEquals(expected,actual);
         }
     }
